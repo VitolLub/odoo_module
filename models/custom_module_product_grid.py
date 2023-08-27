c322a3f584e76a16256fc7d864ef37594ca9bc04
vitollub <vitollub@gmail.com>
2023-08-27 11:47:31 +0300

    Last version

--- models/custom_module_product_grid.py
+++ models/custom_module_product_grid.py
@@ -1,52 +1,57 @@
 from odoo import http,models,fields,api
 import logging,sys
 from datetime import datetime, timedelta
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order'
+    _logger2 = logging.getLogger(__name__)
+    date_planned = fields.Date(string='Expected Arrival', track_visibility='onchange')
+
+    @api.onchange('date_planned')
+    def onchange_date_planned(self):
+        self._logger2.info('onchange_product_id4')
+        for order_line in self:
+            self._logger2.info('onchange_product_id4')
+            product = order_line.product_id.product_tmpl_id
+            self._logger2.info('onchange_product_id4')
+            product._compute_expected_delivery()
+
 class CustomModuleProductGrid(models.Model):
     _logger = logging.getLogger(__name__)
     _inherit = 'product.template'
 
+    expected_delivery = fields.Char(
+        string='Expected Delivery',
+        compute='_compute_expected_delivery',
+        readonly=True,
+        store=True)
 
+    # purchase_order_lines = fields.Many2one(
+    #     "purchase.order",
+    #     string="Purchase Order Lines",
+    # )
 
-    # @api.onchange('expected_delivery')
-    # def onchange_expected_delivery(self):
-    #     # get new date from
+    purchase_order_lines = fields.Many2one(
+        'purchase.order',
+        string='Purchase Order Lines'
+    )
 
-
-    def _compute_expected_delivery(self):
-        # date_planned
-        # for product in self:
-        #     # get all name from purchase.order
-        #     if product.default_code != False:
-        #         full_product_name = product.default_code + " " + product.name
-        #     else:
-        #         full_product_name = product.name
-        #
-        #     # get only name field
-        #     all_name_field = self.env['purchase.order'].search([])
-        #
-        #     for name in all_name_field:
-        #         # get all product_id from purchase.order
-        #
-        #         for order_line in name.order_line:
-        #             product2 = order_line.product_id
-        #             product_name = product2.name
-        #             product_reference = product2.default_code
-        #             if product_reference != False:
-        #                 if full_product_name == product_reference + " " + product_name:
-        #                     # rewrite date_planned
-        #                     self._logger.error(f"Product date_planned: {name.date_planned}")
-        #                     self._logger.error(f"Product id: {product.id}")
-        #                     self._logger.error(f"Product name: {product.name}")
-        #                     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        #                     product.expected_delivery = name.date_planned
-        #                     break
-
+    @api.onchange('purchase_order_lines')
+    def _onchange_purchase_order_lines(self):
+        self._logger.info('onchange_product_id3')
         for product in self:
-            full_product_name = product.default_code + " " + product.name if product.default_code else product.name
+            # Update the expected_delivery based on the related purchase order lines
+            product.expected_delivery = product.purchase_order_lines.order_id.date_planned
+        self._logger.info('onchange_product_id4')
+    @api.depends('purchase_order_lines.date_planned')
+    def _compute_expected_delivery(self):
+        for product in self:
+
             if product.default_code != False:
                 matching_orders = self._search_expected_delivery('default_code', product.default_code)
             else:
                 matching_orders = self._search_expected_delivery('name', product.name)
+
 
             expected_delivery_dates = matching_orders.mapped('order_id.date_planned')
             if expected_delivery_dates:
@@ -61,10 +66,8 @@ class CustomModuleProductGrid(models.Model):
         ])
         return matching_orders
 
-    expected_delivery = fields.Char(
-        string='Expected Delivery',
-        compute='_compute_expected_delivery',
-        readonly=True,
-        store=True)
+
+
+
 
 
